78b28dd94da3cc5c6be0c4530ff3469a8276ea88
Naveen kumar A M <56066312+naveen570@users.noreply.github.com>
2022-01-13 23:27:02 +0530

    added animation till nav toggle

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+let controller;
+let slideScene;
+let pageScene;
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 function animateSlides() {
+  controller = new ScrollMagic.Controller();
   const slides = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
-  slides.forEach((slide) => {
+  slides.forEach((slide, index, slides) => {
     const revealText = slide.querySelector(".reveal-text");
     const revealImg = slide.querySelector(".reveal-image");
     const img = slide.querySelector("img");
@@ -11,13 +18,90 @@ function animateSlides() {
     const slideTl = gsap.timeline({
       defaults: {
         duration: 1,
-        ease: Power4.ease,
+        ease: Power2.ease,
       },
     });
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "=-1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "=-.65");
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "=-0.8");
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      // .addIndicators({
+      //   name: "slide",
+      //   colorStart: "white",
+      //   colorTrigger: "red",
+      // })
+      .addTo(controller);
+    const pageTl = gsap.timeline();
+    const nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageTl.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      // .addIndicators({
+      //   name: "page",
+      //   colorStart: "white",
+      //   colorTrigger: "red",
+      //   colorEnd: "white",
+      //   indent: 200,
+      // })
+      .addTo(controller);
   });
 }
 animateSlides();
+window.addEventListener("mousemove", addCursor);
+window.addEventListener("mouseover", activeCursor);
+burger.addEventListener("click", navToggler);
+function addCursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+function activeCursor(e) {
+  if (
+    e.target.classList.contains("logo") ||
+    e.target.classList.contains("burger")
+  ) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (e.target.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    mouseText.innerText = "Tap";
+    gsap.to(".title-swipe", 0.3, { y: "0%" });
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseText.innerText = "";
+    gsap.to(".title-swipe", 0.3, { y: "100%" });
+  }
+}
+function navToggler(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    document.body.classList.add("hide");
+    gsap.to(".line1", 0.5, { rotate: 45, y: 6, backgroundColor: "black" });
+    gsap.to(".line2", 0.5, { rotate: -45, y: -7, backgroundColor: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    gsap.to(".logo", 1, { color: "black" });
+    mouse.style.border = "2px solid var(--bg-color)";
+  } else {
+    e.target.classList.remove("active");
+    document.body.classList.remove("hide");
+    gsap.to(".line1", 0.5, { rotate: 0, y: 0, backgroundColor: "white" });
+    gsap.to(".line2", 0.5, { rotate: 0, y: 0, backgroundColor: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    gsap.to(".logo", 1, { color: "white" });
+    mouse.style.border = "2px solid var(--primary-color)";
+  }
+}
